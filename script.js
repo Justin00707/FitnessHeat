3d2dd8cb8ecb71780bb4dbc083825e6d246a5629
Justin00707 <Justinsauv@gmail.com>
2024-01-10 21:49:21 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('login').addEventListener('click', function() {
+    alert('Connexion en cours!');
+});
+
+document.getElementById('signup').addEventListener('click', function() {
+    alert('Inscription en cours!');
+});
